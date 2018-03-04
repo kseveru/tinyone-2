79894c0661194a39f0550829e5d71525f7cf226d
Olga <kseveru@yandex.ru>
2018-03-04 20:48:32 +0300

    Исправлена видимость менб без js

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,20 @@
-var navLink = document.querySelector(".btn-show");
-var navPopup = document.querySelector(".site-nav__list");
+var navToggle = document.querySelector(".site-nav__btn");
+var navMain = document.querySelector(".site-nav__list");
 
-if (navLink) {
+navToggle.classList.remove("nojs");
+navMain.classList.remove("nojs");
 
-  navLink.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    navLink.classList.toggle("btn-close");
-    navPopup.classList.toggle("site-nav__list--show");
-  });
+navToggle.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  navToggle.classList.toggle("site-nav__btn--close");
+  navMain.classList.toggle("site-nav__list--opened");
+});
 
-}
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (navToggle.classList.contains("site-nav__btn--close")) {
+      navToggle.classList.remove("site-nav__btn--close");
+      navMain.classList.remove("site-nav__list--opened");
+    }
+  }
+});
